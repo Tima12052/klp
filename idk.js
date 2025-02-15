80e817a5b14ff32f8f905fd98eeb57f7d9a592f0
Tima12052 <161930891+Tima12052@users.noreply.github.com>
2025-02-15 11:13:52 +0200

    234

--- idk.js
+++ idk.js
@@ -6,29 +6,3 @@ let UserState = prompt("Whats your state?");
 let Email = prompt("Whats your Email?");
 let Password = prompt("Whats your Password?");
 let ConfirmPassword = prompt("Confirm your Password");
-
-const userData = {
-    firstName: FirstName,
-    secondName: SecondName,
-    age: Age,
-    userCountry: UserCountry,
-    userState: UserState,
-    email: Email,
-    password: Password,
-    confirmPassword: ConfirmPassword
-};
-
-fetch('tima12052.github.io/klp/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-})
-.then(response => response.json())
-.then(data => {
-    console.log('Success:', data);
-})
-.catch((error) => {
-    console.error('Error:', error);
-});
