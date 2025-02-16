b220d83c7816bf39c8e5888a6fbad8df0d9a8e2f
Tima12052 <161930891+Tima12052@users.noreply.github.com>
2025-02-16 21:40:32 +0200

    123

--- idk.js
+++ idk.js
@@ -1,8 +1,4 @@
-let FirstName = prompt("Whats your first name?");
-let SecondName = prompt("Whats your second name?");
-let Age = prompt("How old are you?");
-let UserCountry = prompt("Whats your country?");
-let UserState = prompt("Whats your state?");
-let Email = prompt("Whats your Email?");
-let Password = prompt("Whats your Password?");
-let ConfirmPassword = prompt("Confirm your Password");
+const x = Number(prompt("Введіть перше число:"));
+const y = Number(prompt("Введіть друге число:"));
+
+alert(x + " * " + y + " = " + (x * y));
