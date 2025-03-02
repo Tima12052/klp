4f19632598bbc17c4ea8d7712bdea379afcc3f62
Tima12052 <161930891+Tima12052@users.noreply.github.com>
2025-03-02 10:34:45 +0200

    123

--- idk.js
+++ idk.js
@@ -1,4 +1,30 @@
-const x = Number(prompt("Введіть перше число:"));
-const y = Number(prompt("Введіть друге число:"));
+// const x = Number(prompt("Введіть перше число:"));
+// const y = Number(prompt("Введіть друге число:"));
 
-alert(x + " * " + y + " = " + (x * y));
+// alert(x + " * " + y + " = " + (x * y));
+
+
+// const message = 'Скачать' + ' обои' + ' клещь' + ' рояль!!!';
+// console.log(message);
+// setTimeout(() => {
+//     console.log();
+// }, 4000);
+
+
+// const name = 'Oamt';
+// const age = 999;
+// const mood = 'Javascript is awesome!';
+
+// const sameMessage = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+// console.log(sameMessage);
+// let x = "Oamt";
+// let y = "Malbuk";
+// let n = 666;
+// let g = 777;
+// console.log("Гість ", x, y, "поселяється в ", n, "номер ", g);
+// // Copyright ©
+
+// const mesig = prompt('type your message');
+// console.log(mesig.trim());
+
+
